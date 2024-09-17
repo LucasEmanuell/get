ebcdccc4fd7e42b6e732df4c5a97373a433f4f5a
Lucas <lucasemanuellq@gmail.com>
2024-09-17 01:23:14 -0300

    add vrp solution plus map build in jupyter

--- get_position.py
+++ get_position.py
@@ -3,7 +3,7 @@ from geopy.geocoders import Nominatim
 from geopy.extra.rate_limiter import RateLimiter
 
 # Leitura do CSV com pandas
-df = pd.read_csv('C:\\Users\\neumann\\Documents\\projects\\get\\restaurantes_fortaleza.csv')  # Substitua 'enderecos.csv' pelo nome do seu arquivo CSV
+df = pd.read_csv('C:\\Users\\neumann\\Documents\\projects\\get\\fixo.csv')  # Substitua 'enderecos.csv' pelo nome do seu arquivo CSV
 
 # Inicializa o geocodificador
 geolocator = Nominatim(user_agent="geoapiExercises")
@@ -22,4 +22,4 @@ def get_lat_long(endereco):
 # Aplica a função de geocodificação à coluna de endereços
 df['Latitude'], df['Longitude'] = zip(*df['Endereço'].apply(get_lat_long))
 # Salva o novo CSV com o nome, latitude e longitude
-df[['Nome', 'Latitude', 'Longitude']].to_csv('C:\\Users\\neumann\\Documents\\projects\\get\\restaurantes_position.csv', index=False)
+df[['Nome', 'Latitude', 'Longitude']].to_csv('C:\\Users\\neumann\\Documents\\projects\\get\\fixo_position.csv', index=False)
